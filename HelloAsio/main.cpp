481fd53214b6a9757960413844dedcb370cbbac2
Michael Jones <mikej253@me.com>
2015-07-26 08:29:49 +1000

    Initial Commit

---
+++ HelloAsio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloAsio
+//
+//  Created by Michael Jones on 26/07/2015.
+//  Copyright (c) 2015 Michael Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
